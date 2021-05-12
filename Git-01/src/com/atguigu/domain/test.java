a280dc111d511bf99103796ae1b7e6551ce38d89
yh-com <1907120040@qq.com>
2021-05-12 11:07:14 +0800

    汉哥第一次提交代码

---
+++ Git-01/src/com/atguigu/domain/test.java
@@ -0,0 +1,20 @@
+package com.atguigu.domain;
+
+public class test {
+
+    public static void add(){
+
+    }
+    public static void main(String[] args) {
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+        System.out.println("汉哥，牛逼。。。。。。。。");
+    }
+}
